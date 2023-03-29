d01fb9f5c8d6324af4c03103cef9a4540729140e
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-03-29 13:00:26 +0530

    dynamic routes adding a product detais page

--- models/product.js
+++ models/product.js
@@ -18,11 +18,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
+    this.id = Math.random().toString()
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -34,4 +38,12 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
+
+
+  static findById(id, cb) {
+  getProductsFromFile(products=>{
+      const product = products.find(p=>p.id === id)
+      cb(product)
+  })
+ }
 };
