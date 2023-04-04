8c24403d6d14c12e9502cb215721f7b704f5526b
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-04-04 22:59:44 +0530

    using sequelize and integrating it to project

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -22,5 +22,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync().then(app.listen(3000))
+.catch(err=>console.log(err))
 
-app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,10 +1,7 @@
-const mysql =  require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Sharpenarian1998'
-})
-
-module.exports = pool.promise()
+const sequelize = new Sequelize('node-complete', 'root', 'Sharpenarian1998',{
+    dialect: 'mysql',
+    host: 'localhost'
+} )
+module.exports = sequelize;
