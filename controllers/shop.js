ba76e1dcb4e789eb5809954a7b579981b6ace5d3
ShubhamSatyabola <satyabolashubham@gmail.com>
2023-04-16 11:15:32 +0530

    association , implementing cart functionalities

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,19 +48,71 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
+  req.user
+  .getCart()
+  .then(cart=>{
+    return cart.getProducts()
+    .then(products=>{
+      res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart'
+    pageTitle: 'Your Cart',
+    products: products
   });
+    })
+    .catch(err=>console.log(err))
+  })
+  .catch(err=>console.log(err))
+  
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId,(product) => {
-    Cart.addProduct(prodId, product.price)
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user.getCart()
+  .then(cart=>{
+    fetchedCart = cart;
+    return cart.getProducts({where: {id: prodId}})
+  }).then(products=>{
+    let product;
+    if(products.length>0){
+      product=products[0];
+    }
+    
+    if (product){
+      
+      const oldQuantity = product.cartItem.quantity; 
+      newQuantity = oldQuantity + 1;
+    }
+    return Product.findByPk(prodId)
+    .then(product=>{
+      return fetchedCart.addProduct(product, { through: {quantity: newQuantity}});
+    })
+  })
+  .then(()=>{
+    res.redirect('/cart');
+  })
+  .catch(err=>console.log(err))
+ 
+  
 }
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.getCart()
+  .then(cart=>{
+    return cart.getProducts({where: {id: prodId}})
+  })
+  .then(products=>{
+    const product = products[0];
+    return product.cartItem.destroy();
+  })
+  .then(result=>{
+    res.redirect('/cart');
+  })
+  .catch(err=>console.log(err))
+ 
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
